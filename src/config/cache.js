8acd20a4a1ec4dc564fa7e3cf384067704eee38d
saurabh kashyap <saurabhkashyap0001@gmail.com>
2021-03-20 14:40:21 +0530

    update the catches seprated object for config connection

--- src/config/cache.js
+++ src/config/cache.js
@@ -10,6 +10,6 @@ export const REDIS_OPTIONS = {
   // host: REDIS_HOST,
   // password: REDIS_PASSWORD
   host: 'redis-16610.c244.us-east-1-2.ec2.cloud.redislabs.com',
-  port:16610
+  port:16610,
   password: 'HOXm5SfHNIyNB8IceXZF5c6ie5Rb6RYg'
 };
