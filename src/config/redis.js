45df6a22c099a6db8f39aa4a876be5cdc4d1cdf7
saurabh kashyap <saurabhkashyap0001@gmail.com>
2021-03-20 15:23:58 +0530

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,8 @@
 export default{
     //host: process.env.REDIS_HOST,
-    //port: process.env.REDIS_PORT,
+     //port: process.env.REDIS_PORT,
     //password: process.env.REDIS_PASSWORD
-    host: 'redis-16610.c244.us-east-1-2.ec2.cloud.redislabs.com',
-    port:16610,
+    host: 'redis-16610.c244.us-east-1-2.ec2.cloud.redislabs.com:16610',
+    //port:16610,
     password: 'HOXm5SfHNIyNB8IceXZF5c6ie5Rb6RYg'
   };
