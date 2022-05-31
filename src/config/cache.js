7755e8643361f0a226136b771f859d6bce92bdeb
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2022-05-31 23:55:01 +0530

    :bug: fix redis connection option with enviroment veriable

--- src/config/cache.js
+++ src/config/cache.js
@@ -1,9 +1,9 @@
 
-// const {
-//   REDIS_PORT = 17102,
-//   REDIS_HOST = "redis-17102.c212.ap-south-1-1.ec2.cloud.redislabs.com",
-//   REDIS_PASSWORD = "o2VTQjWB1oLh3Q2Rq0slCT0zTV8ATkKm"
-// } = process.env;
+const {
+  REDIS_PORT = process.env.REDIS_PORT,
+  REDIS_HOST = process.env.REDIS_HOST,
+  REDIS_PASSWORD = process.env._REDIS_PASSWORD
+} = process.env;
 // import { RedisMemoryServer } from 'redis-memory-server';
 
 // const redisServer = new RedisMemoryServer();
@@ -12,10 +12,10 @@
 // const port = await redisServer.getPort();
 // console.log(`${redisServer.getInstanceInfo()}`)
 export const REDIS_OPTIONS = {
-  // port: +REDIS_PORT,
-  // host: REDIS_HOST,
-  // password: REDIS_PASSWORD
-  host: '127.0.0.1' ||host_redis,
-  port: 6379 ||port_redis
+  port: REDIS_PORT,
+  host: REDIS_HOST,
+  password: REDIS_PASSWORD
+  // host: '127.0.0.1' ||host_redis,
+  // port: 6379 ||port_redis
   // password: 'o2VTQjWB1oLh3Q2Rq0slCT0zTV8ATkKm'
 };
